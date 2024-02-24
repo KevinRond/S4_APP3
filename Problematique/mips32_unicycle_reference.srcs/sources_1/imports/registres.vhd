1083361b6d3cac9e1a4891d6c64c85bc25a0c08c
Kevin <ronk2602@usherbrooke.ca>
2024-02-24 18:01:43 -0500

    GAAAAAAAB

--- Problematique/mips32_unicycle_reference.srcs/sources_1/imports/registres.vhd
+++ Problematique/mips32_unicycle_reference.srcs/sources_1/imports/registres.vhd
@@ -22,22 +22,24 @@ entity BancRegistres is
            reset     : in  std_logic;
            i_RS1     : in  std_logic_vector (4 downto 0);
            i_RS2     : in  std_logic_vector (4 downto 0);
-           i_Wr_DAT  : in  std_logic_vector (31 downto 0);
+           i_Wr_DAT  : in  std_logic_vector (127 downto 0);
            i_WDest   : in  std_logic_vector (4 downto 0);
            i_WE 	 : in  std_logic;
-           o_RS1_DAT : out std_logic_vector (31 downto 0);
-           o_RS2_DAT : out std_logic_vector (31 downto 0));
+           o_RS1_DAT : out std_logic_vector (127 downto 0);
+           o_RS2_DAT : out std_logic_vector (127 downto 0));
 end BancRegistres;
 
 architecture comport of BancRegistres is
     signal regs: RAM(0 to 31) := (29 => X"100103FC", -- registre $SP
                                 others => (others => '0'));
+    signal vregs: VRAM(0 to 7) := (others => (others => '0'));
 begin
     process( clk )
     begin
         if clk='1' and clk'event then
             if i_WE = '1' and reset = '0' and i_WDest /= "00000" then
                 regs( to_integer( unsigned(i_WDest))) <= i_Wr_DAT;
+                if i_WDEST >
             end if;
         end if;
     end process;
